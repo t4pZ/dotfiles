52a0cac3407f711294390a55470f4ed56f96ee31
t4pZ <t4pZ0r@gmail.com>
2024-01-06 00:30:07 +0100

    execute nvidia settins after login

---
+++ .local/bin/nvidia.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/usr/bin/nvidia-settings --assign CurrentMetaMode="DPY-1: nvidia-auto-select @2560x1440 +0+0 {ViewPortIn=2560x1440, ViewPortOut=2560x1440+0+0, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DPY-3: nvidia-auto-select @2560x1440 +2560+0 {ViewPortIn=2560x1440, ViewPortOut=2560x1440+0+0, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On }"
